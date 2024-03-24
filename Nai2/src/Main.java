a8bd7ef40981f0fba9f4a7f23cc606c3af703984
Jakub <kubakarwow@gmail.com>
2024-03-24 11:06:16 +0100

    implemented testing

--- Nai2/src/Main.java
+++ Nai2/src/Main.java
@@ -6,9 +6,20 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<String> strings = Files.readAllLines(Paths.get("iris_training 3.txt"));
-        List<DataRow> rows = strings.stream().map(s -> new DataRow(s)).toList();
+        List<String> training = Files.readAllLines(Paths.get("iris_training 3.txt"));
+        List<DataRow> rows = training.stream().map(s -> new DataRow(s)).toList();
+
+        List<String> test = Files.readAllLines(Paths.get("iris_test 3.txt"));
+        List<DataRow> testRows = test.stream().map(s -> new DataRow(s)).collect(Collectors.toList());
+
         Perceptor perceptor = new Perceptor(rows);
         perceptor.train();
+        int amountOfCheckedOnes=0;
+        for(DataRow row: testRows){
+            boolean isGood = perceptor.test(row);
+            if(isGood) amountOfCheckedOnes++;
+        }
+        System.out.println("out of "+testRows.size() + " test rows, "+amountOfCheckedOnes+" were correct");
+        System.out.println("algorithm works " + (amountOfCheckedOnes*100.0)/testRows.size()+"% of the time");
     }
 }
--- Nai2/src/Perceptor.java
+++ Nai2/src/Perceptor.java
@@ -16,53 +16,69 @@ public class Perceptor {
     // mamy wektory X na końcu -1
     // wzor to W'= W - (d-y)L*X
     // L=1
-    Perceptor(List<DataRow> training){
-        this.training=training;
-        weights= new double[training.get(0).numericAttributes.length+1];
+    Perceptor(List<DataRow> training) {
+        this.training = training;
+        weights = new double[training.get(0).numericAttributes.length + 1];
     }
-    public void train(){
-        for(int i=0; i<500; i++){
-            boolean isEverythingGood=true;
-            for(DataRow row : training){
+
+    public boolean test(DataRow row) {
+        double skalar = 0.0;
+        for (int j = 0; j < weights.length - 1; j++) {
+            skalar += weights[j] * row.numericAttributes[j];
+        }
+        if (skalar >= weights[weights.length - 1]){
+            return row.resultAttribute.equals("Iris-setosa");
+        } else {
+            return !row.resultAttribute.equals("Iris-setosa");
+        }
+
+    }
+
+    public void train() {
+        for (int i = 0; i < 500; i++) {
+            boolean isEverythingGood = true;
+            for (DataRow row : training) {
                 System.out.println("--------------------NEW ROW-------------------");
                 System.out.println(this);
                 System.out.println(row);
                 boolean b = processRow(row);
                 System.out.println(b);
-                if(!b) isEverythingGood=false;
+                if (!b) isEverythingGood = false;
             }
-            if (isEverythingGood){
+            if (isEverythingGood) {
                 System.out.println(i);
                 break;
             }
         }
     }
-    public boolean processRow(DataRow row){
+
+    public boolean processRow(DataRow row) {
         double skalar = 0.0;
-        int actualResult=-1;
-        int properResult=-1;
-        for(int i=0; i<row.numericAttributes.length; i++){
-            skalar+=weights[i]*row.numericAttributes[i];
+        int actualResult = -1;
+        int properResult = -1;
+        for (int i = 0; i < row.numericAttributes.length; i++) {
+            skalar += weights[i] * row.numericAttributes[i];
         }
-        skalar+=weights[weights.length-1]*-1;
-        System.out.println("SKALAR:"+skalar);
-        actualResult = skalar>=weights[weights.length-1]?1:0;
-        System.out.println("ACTUAL RESULT:"+actualResult);
-        properResult=row.resultAttribute.equals("Iris-setosa")?1:0;
-        System.out.println("PROPER RESULT:"+properResult);
-        adjustWeights(properResult,actualResult,row);
-        if(!(actualResult==properResult)) processRow(row);
-        return actualResult==properResult;
+        skalar += weights[weights.length - 1] * -1;
+        System.out.println("SKALAR:" + skalar);
+        actualResult = skalar >= weights[weights.length - 1] ? 1 : 0;
+        System.out.println("ACTUAL RESULT:" + actualResult);
+        properResult = row.resultAttribute.equals("Iris-setosa") ? 1 : 0;
+        System.out.println("PROPER RESULT:" + properResult);
+        adjustWeights(properResult, actualResult, row);
+        if (!(actualResult == properResult)) processRow(row);
+        return actualResult == properResult;
 
     }
-    public void adjustWeights(int properResult,int actualResult, DataRow dataRow){
-        if(properResult==actualResult) return;
-        double[] newWeights= new double[weights.length];
-        for(int i=0; i<weights.length-1; i++){
-            newWeights[i]=weights[i]+(properResult-actualResult)*dataRow.numericAttributes[i];
+
+    public void adjustWeights(int properResult, int actualResult, DataRow dataRow) {
+        if (properResult == actualResult) return;
+        double[] newWeights = new double[weights.length];
+        for (int i = 0; i < weights.length - 1; i++) {
+            newWeights[i] = weights[i] + (properResult - actualResult) * dataRow.numericAttributes[i];
         }
-        newWeights[newWeights.length-1]=weights[weights.length-1]+(properResult-actualResult)*-1;
-        weights=newWeights;
+        newWeights[newWeights.length - 1] = weights[weights.length - 1] + (properResult - actualResult) * -1;
+        weights = newWeights;
     }
 
 }
